0c3e0ed177c8c8b516b33df11606ca3ae50b21c2
yuta-yamaura <shk0fate@gmail.com>
2023-11-13 20:47:22 +0900

    WebToDoStarter-2 commit

---
+++ main/java/com/example/demo/config/WebMvcControllerAdvice.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+/**
+ * こちらは今回使用しません。
+ * ルートでアクセスしたときにtest.htmlを表示
+ */
+//@Controller
+//@RequestMapping("/")
+//public class SampleController {
+//	
+//	@GetMapping
+//	public String test() {
+//		return "test";
+//	}
+//
+//}
