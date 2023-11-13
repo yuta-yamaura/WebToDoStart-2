4a8c97a791a43612b9ba8a867dbbca3d52e0e709
yuta-yamaura <shk0fate@gmail.com>
2023-11-13 21:06:24 +0900

    TaskServiceImpl.javaのEmptyResultDataAccessExceptionをインポート

--- main/java/com/example/demo/service/TaskServiceImpl.java
+++ main/java/com/example/demo/service/TaskServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.TaskDao;
